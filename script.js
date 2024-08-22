5c71d945374f5c43fa3bf6af23e0acf2fdfa05ce
GUSTAVO MUZEKA <gustavo@suzuki.dominio>
2024-08-22 16:08:54 -0300

    Atualização colocando try e catch.

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const containerVideos = document.querySelector('.videos__container')
 async function buscarEMostrarVideos() {
-    
-    const busca = await fetch("http://localhost:3000/videos");
-    const videos = await busca.json();
-        videos.forEach((video)=>{ 
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+            videos.forEach((video)=>{ 
+                if(video.categoria == ""){
+                    throw new Error ("Video Não tem Categoria")
+                }
                 containerVideos.innerHTML += `
-                <li class="videos__item">
-                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-                    <div class="descricao-video">
-                        <img class="img-canal" src="${video.imagem}" alt="Logo Canal" >
-                        <h3 class="titulo-video">${video.titulo}</h3>
-                        <p class="titulo-canal">${video.descricao}</p>
-                    </div>
-                </li>
-                `;
-            })
-
+                    <li class="videos__item">
+                        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                        <div class="descricao-video">
+                            <img class="img-canal" src="${video.imagem}" alt="Logo Canal" >
+                            <h3 class="titulo-video">${video.titulo}</h3>
+                            <p class="titulo-canal">${video.descricao}</p>
+                        </div>
+                    </li>
+                    `;
+                })
+        }catch(error){
+            containerVideos.innerHTML = `houve um erro ao carregar os videos ${error}`
+        }
 }
 
 buscarEMostrarVideos();
