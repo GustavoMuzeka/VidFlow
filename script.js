542693dc5932aa786b302241b900081e1f95cc4a
GUSTAVO MUZEKA <gustavo@suzuki.dominio>
2024-08-22 16:00:38 -0300

    Aprimorando com Async

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const containerVideos = document.querySelector('.videos__container')
+async function buscarEMostrarVideos() {
+    
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
+        videos.forEach((video)=>{ 
+                containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo Canal" >
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+                `;
+            })
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((videos) =>
-    videos.forEach((video)=>{ 
-        containerVideos.innerHTML += `
-        <li class="videos__item">
-            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-            <div class="descricao-video">
-                <img class="img-canal" src="${video.imagem}" alt="Logo Canal" >
-                <h3 class="titulo-video">${video.titulo}</h3>
-                <p class="titulo-canal">${video.descricao}</p>
-            </div>
-        </li>
-        `;
-    })
+}
 
-)
-.catch((error) =>{
-    containerVideos.innerHTML = `<p> houve um erro ao carregar os vídeos: ${error}</p>`;
-})
-
+buscarEMostrarVideos();
